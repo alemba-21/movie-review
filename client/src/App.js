078608b5009e84a7b16d90750929d031c6d28685
alemba-21 <alembakiragu@gmail.com>
2022-09-15 08:00:36 +0300

    i'm giving it a classname of App

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,37 @@ function App(){
     
 
   return (
-  
+    
+      <h1>Movie Review</h1>
+
+      <div className='form'>
+      <label>Movie Name:</label>
+      <input type="text" name='movieName' onChange={(e)=>{
+        setMovieName(e.target.value)
+      }} />
+      <label>Review:</label>
+      <input type="text" name='review' onChange={(e)=> {
+        setReview(e.target.value)
+      }}/>
+
+      <button onClick={submitReview}>Submit</button>
+
+      {movieReviewList.map((val)=>{
+        return (
+        <div className='card'>
+          <h1>{val.movieName}</h1>
+          <p>{val.movieReview}</p>
+
+          <button onClick={() => {deleteReview(val.movieName)}}>Delete</button>
+          <input type="text" id='updateInput' onChange={(e)=>{
+            setNewReview(e.target.value)
+          }}/>
+          <button onClick={()=> {updateReview(val.movieName)}}>Update</button>
+          </div>
+          );
+      })}
+      </div>
+    </div>
   )
 }
 
