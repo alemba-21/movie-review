7016df0da27cde4e32a1e1565491c586d858ea8c
alemba-21 <alembakiragu@gmail.com>
2022-09-15 07:59:41 +0300

    structuring the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -32,41 +32,18 @@ function App(){
     };
 
 
-   
+    const updateReview = (movie) =>{
+      Axios.delete("http://localhost:3001/api/update", {
+        movieName: movie, 
+        movieReview: newReview,
+      });
+      setNewReview("")
+    };
+
     
 
   return (
-    <div className='App'>
-      <h1>Movie Review</h1>
-
-      <div className='form'>
-      <label>Movie Name:</label>
-      <input type="text" name='movieName' onChange={(e)=>{
-        setMovieName(e.target.value)
-      }} />
-      <label>Review:</label>
-      <input type="text" name='review' onChange={(e)=> {
-        setReview(e.target.value)
-      }}/>
-
-      <button onClick={submitReview}>Submit</button>
-
-      {movieReviewList.map((val)=>{
-        return (
-        <div className='card'>
-          <h1>{val.movieName}</h1>
-          <p>{val.movieReview}</p>
-
-          <button onClick={() => {deleteReview(val.movieName)}}>Delete</button>
-          <input type="text" id='updateInput' onChange={(e)=>{
-            setNewReview(e.target.value)
-          }}/>
-          <button onClick={()=> {updateReview(val.movieName)}}>Update</button>
-          </div>
-          );
-      })}
-      </div>
-    </div>
+  
   )
 }
 
