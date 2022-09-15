60f386b6a8f1456a57912560cb7dc388e054788a
alemba-21 <alembakiragu@gmail.com>
2022-09-15 08:04:48 +0300

    giving function on input set review

--- client/src/App.js
+++ client/src/App.js
@@ -48,11 +48,11 @@ function App(){
 
       <div className='form'>
       <label>Movie Name:</label>
-      
+      <input type="text" name='movieName' onChange={(e)=>{
+        setMovieName(e.target.value)
+      }} />
       <label>Review:</label>
-      <input type="text" name='review' onChange={(e)=> {
-        setReview(e.target.value)
-      }}/>
+      
 
       <button onClick={submitReview}>Submit</button>
 
