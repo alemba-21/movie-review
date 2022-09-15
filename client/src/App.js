b3e1abbe7e8ecf858b9ecaf717f4e6baef73d4ad
alemba-21 <alembakiragu@gmail.com>
2022-09-15 07:33:34 +0300

    working on the READme

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,46 @@ function App(){
 
   const [movieName, setMovieName] = useState('');  
   const [review, setReview] = useState('');  
+  const[movieReviewList, setMovieList] = useState([]);
+  const [newReview, setNewReview] = useState ("")
+
+  useEffect(() =>{
+    Axios.get('http://localhost:3001/api/get').then((response)=>{
+        setMovieList(response.data)
+    });
+  }, []);
 
   const submitReview = () => {
-    Axios.post('http://localhost:3001/', {movieName: movieName, movieReview: mo})
-  }
+    Axios.post('http://localhost:3001/api/insert', {
+      movieName: movieName, 
+      movieReview: review,
+    });
+      
+    setMovieList([
+      ...movieReviewList, 
+      {movieName: movieName, movieReview: review},
+    ]);
+    };
+
+    const deleteReview = (movie) =>{
+      Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+    };
+
+
+    const updateReview = (movie) =>{
+      Axios.delete("http://localhost:3001/api/update", {
+        movieName: movie, 
+        movieReview: newReview,
+      });
+      setNewReview("")
+    };
+
+    
 
   return (
     <div className='App'>
       <h1>Movie Review</h1>
+
       <div className='form'>
       <label>Movie Name:</label>
       <input type="text" name='movieName' onChange={(e)=>{
@@ -25,6 +57,21 @@ function App(){
       }}/>
 
       <button onClick={submitReview}>Submit</button>
+
+      {movieReviewList.map((val)=>{
+        return (
+        <div className='card'>
+          <h1>{val.movieName}</h1>
+          <p>{val.movieReview}</p>
+
+          <button onClick={() => {deleteReview(val.movieName)}}>Delete</button>
+          <input type="text" id='updateInput' onChange={(e)=>{
+            setNewReview(e.target.value)
+          }}/>
+          <button onClick={()=> {updateReview(val.movieName)}}>Update</button>
+          </div>
+          );
+      })}
       </div>
     </div>
   )
