dfe142d51b0b026bdaf177c9a2f1dff53a6109e7
alemba-21 <alembakiragu@gmail.com>
2022-09-18 16:37:15 +0300

    working on my review submit button which is supposed to paste the info in the feedback section

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,7 +4,18 @@ import feedback from './feedback';
 
 const Review = () => {
   return (
- 
+    <div className='mwili'>
+      <div className="container2">
+        <div className="movie-title">
+        <h5>Movie Title</h5>
+        <input type="text" placeholder='Type movie title'/>
+        </div>
+        <div className="comments">
+          <h5>Review</h5>
+          <textarea type="text" placeholder='whats your feedback?' />
+        </div>
+       
+      </div>
     </div>
   )
 }
